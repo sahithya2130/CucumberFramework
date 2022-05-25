06f6ba8f8ce03afa1b65f1b7a8e08b8635057439
sahithya2130 <sathyadasari1999@gmail.com>
2022-05-25 17:53:15 +0530

    added restAssured automation file

---
+++ src/main/java/git/practice/RestAssuredAuto.java
@@ -0,0 +1,12 @@
+package git.practice;
+
+public class RestAssuredAuto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Api automation using rest assured");
+		System.out.println("Data driven from excel file");
+
+	}
+
+}
