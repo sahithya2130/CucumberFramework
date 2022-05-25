426943d7bc99e50902294b23f32eae1e8351ee27
sahithya2130 <sathyadasari1999@gmail.com>
2022-05-25 17:47:41 +0530

    added selenium automation file

---
+++ src/main/java/git/practice/SeleniumAuto.java
@@ -0,0 +1,12 @@
+package git.practice;
+
+public class SeleniumAuto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Selenium automation");
+		System.out.println("Web automation testing");
+
+	}
+
+}
