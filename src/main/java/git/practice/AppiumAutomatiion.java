ec8edbf17e2e3b405bbf7f23e31c332fec730c39
sahithya2130 <sathyadasari1999@gmail.com>
2022-05-25 18:43:23 +0530

    Architect A commit

---
+++ src/main/java/git/practice/AppiumAutomatiion.java
@@ -0,0 +1,24 @@
+package git.practice;
+
+public class AppiumAutomatiion {
+
+	public static void IOSDevices(){
+		System.out.println("Ios phones");
+	}
+	
+	
+	public static void AndroidDevices(){
+		System.out.println("Android phones");
+	}
+	
+	
+	public static void WindowsDevices(){
+		System.out.println("Windows phones");
+	}
+	
+	
+	public static void LinuxDevices(){
+		System.out.println("Linux phones");
+	}
+	
+}
